fbc29dac3c39d3a1d32065736bd762f667d9cdb3
terry_teague <>
2001-06-02 09:09:14 +0000

    [ #427677 ] TrimInitialSpace() can trim too much

--- tidy/src/parser.c
+++ tidy/src/parser.c
@@ -329,7 +329,7 @@ static void TrimInitialSpace(Lexer *lexer, Node *element, Node *text)
 {
     Node *prev, *node;
 
-    if (text->type == TextNode && lexer->lexbuf[text->start] == ' ')
+    if (text->type == TextNode && lexer->lexbuf[text->start] == ' ' && text->start < text->end)	/* #427677 - fix by Gary Peskin 31 Oct 00 */
     {
         if ((element->tag->model & CM_INLINE) &&
             !(element->tag->model & CM_FIELD) &&
