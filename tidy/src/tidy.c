3b5d9bc668505fde1fd9c72ab82b070e40f883cb
terry_teague <>
2001-09-15 21:31:18 +0000

    Fix for 461643 - Some Shift-JIS characters handled wrong

--- tidy/src/tidy.c
+++ tidy/src/tidy.c
@@ -1041,6 +1041,15 @@ static int ReadCharFromStream(StreamIn *in)
     {
         if (c < 128)
             return c;
+        else if ((in->encoding == SHIFTJIS) && (c >= 0xa1 && c <= 0xdf)) /* 461643 - fix suggested by Rick Cameron 14 Sep 01 */
+        {
+            /*
+              Rick Cameron pointed out that for Shift_JIS, the values from
+              0xa1 through 0xdf represent singe-byte characters
+              (U+FF61 to U+FF9F - half-shift Katakana)
+            */
+            return c;
+        }
         else
         {
             uint c1;
