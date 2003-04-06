c664e18040240fee3c76ca396a816a1b621f2997
hoehrmann <>
2003-04-06 20:35:58 +0000

    fix for 639887, in -xml id attributes aren't necessarily of type ID

--- tidy/src/lexer.c
+++ tidy/src/lexer.c
@@ -3401,8 +3401,8 @@ static tmbstr ParseValue( TidyDocImpl* doc, ctmbstr name,
 
         if (c == '&')
         {
-            /* no entities in ID attributes */
-            if (tmbstrcasecmp(name, "id") == 0)
+            /* no entities in ID attributes for XHTML/HTML */
+            if (tmbstrcasecmp(name, "id") == 0 && !cfgBool(doc, TidyXmlTags))
             {
                 ReportAttrError( doc, NULL, NULL, ENTITY_IN_ID );
                 continue;
