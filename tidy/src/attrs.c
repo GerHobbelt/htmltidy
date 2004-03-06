650c4fc1fb4f5d8ec1e94934c42c1d2057dd26f7
hoehrmann <>
2004-03-06 19:58:32 +0000

    fix for http://tidy.sf.net/bug/878803

--- tidy/src/attrs.c
+++ tidy/src/attrs.c
@@ -827,7 +827,7 @@ void RepairDuplicateAttributes( TidyDocImpl* doc, Node *node)
             /* first and second attribute have same local name */
             /* now determine what to do with this duplicate... */
 
-            if (attrIsCLASS(first) && cfgBool(doc, TidyJoinClasses))
+            if (attrIsCLASS(first) && cfgBool(doc, TidyJoinClasses) && AttrHasValue(first))
             {
                 /* concatenate classes */
 
@@ -843,7 +843,7 @@ void RepairDuplicateAttributes( TidyDocImpl* doc, Node *node)
 
                 second = temp;
             }
-            else if (attrIsSTYLE(first) && cfgBool(doc, TidyJoinStyles))
+            else if (attrIsSTYLE(first) && cfgBool(doc, TidyJoinStyles) && AttrHasValue(first))
             {
                 /* concatenate styles */
 
