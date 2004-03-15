dd1c6ca9947c60c959ea51071d52a7b279c89905
hoehrmann <>
2004-03-15 04:48:46 +0000

    improved handling for some weird case where there is a <script> inside a <script> without inner </script>

--- tidy/src/parser.c
+++ tidy/src/parser.c
@@ -2976,8 +2976,10 @@ void ParseScript(TidyDocImpl* doc, Node *script, uint mode)
 
     if (!(node && node->type == EndTag && node->tag->id == script->tag->id))
     {
-        ReportError(doc, script, node, MISSING_ENDTAG_BEFORE);
-        UngetToken(doc);
+        ReportError(doc, script, node, MISSING_ENDTAG_FOR);
+
+        if (node)
+            UngetToken(doc);
     }
     else
     {
