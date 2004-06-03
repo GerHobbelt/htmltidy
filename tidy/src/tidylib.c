095e089ad52ee81b3652c2629a51ece40f863945
hoehrmann <>
2004-06-03 14:25:24 +0000

    drop obsolete code

--- tidy/src/tidylib.c
+++ tidy/src/tidylib.c
@@ -962,16 +962,6 @@ int         tidyDocSaveStdout( TidyDocImpl* doc )
 #if defined(_WIN32) || defined(OS2_OS)
     oldstdoutmode = setmode( fileno(stdout), _O_BINARY );
     oldstderrmode = setmode( fileno(stderr), _O_BINARY );
-
-#if SUPPORT_UTF16_ENCODINGS && 0 /* fix for bug 723948 */
-    if ( out->encoding == UTF16   ||
-         out->encoding == UTF16LE ||
-         out->encoding == UTF16BE )
-    {
-      ReportError(doc, NULL, doc->root, ENCODING_IO_CONFLICT );
-    }
-#endif
-
 #endif
 
 #endif
