b879161bd5a548f7b6e451dc4bcf9478a565d57c
arnaud02 <>
2005-02-22 13:09:39 +0000

    Fix for http://tidy.sf.net/bug/1052758.

--- tidy/src/clean.c
+++ tidy/src/clean.c
@@ -2473,11 +2473,15 @@ void FixLanguageInformation(TidyDocImpl* doc, Node* node, Bool wantXmlLang, Bool
             }
             else if (lang && wantXmlLang)
             {
-                RepairAttrValue(doc, node, "xml:lang", lang->value);
+                if (NodeAttributeVersions( node, TidyAttr_XML_LANG )
+                    & doc->lexer->versionEmitted)
+                    RepairAttrValue(doc, node, "xml:lang", lang->value);
             }
             else if (xmlLang && wantLang)
             {
-                RepairAttrValue(doc, node, "lang", xmlLang->value);
+                if (NodeAttributeVersions( node, TidyAttr_LANG )
+                    & doc->lexer->versionEmitted)
+                    RepairAttrValue(doc, node, "lang", xmlLang->value);
             }
 
             if (lang && !wantLang)
