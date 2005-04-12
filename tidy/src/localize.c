22ef96b65a48a835ee42da6e65b16cb50851d5b8
arnaud02 <>
2005-04-12 11:58:56 +0000

    Update release date to 12 April 2005

--- tidy/src/localize.c
+++ tidy/src/localize.c
@@ -30,7 +30,7 @@
 */
 #define ATRC_ACCESS_URL  "http://www.aprompt.ca/Tidy/accessibilitychecks.html"
 
-static const char *release_date = "1st March 2005";
+static const char release_date[] = "12 April 2005";
 
 ctmbstr ReleaseDate(void)
 {
